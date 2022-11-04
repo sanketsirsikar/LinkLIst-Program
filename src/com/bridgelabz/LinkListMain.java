1a21a072a86f46e00db569ef2346c85642e85a92
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-11-05 00:55:02 +0530

    create list added

--- src/com/bridgelabz/LinkListMain.java
+++ src/com/bridgelabz/LinkListMain.java
@@ -3,5 +3,6 @@ package com.bridgelabz;
 public class LinkListMain {
     public static void main (String [] args){
         System.out.println("Welcome to Linklist Program ");
+        System.out.println("Create a simple Linklist of 56,30 and 70");
     }
 }
